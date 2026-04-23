5adaeeded5bae921d9b066ed33e511c589d4d142
StudioLee-cmd <Tim@studiolee.nl>
2026-04-23 22:37:14 +0200

    fix(hero): trapper -> lekke band (origineel)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -75,7 +75,7 @@ const Hero: React.FC = () => {
                         <motion.span
                             style={{ x: xLeft, opacity, display: 'inline-block' }}
                         >
-                            <Highlight color="secondary">trapper</Highlight>.
+                            <Highlight color="secondary">lekke band</Highlight>.
                         </motion.span>
                     </h1>
                 </motion.div>
